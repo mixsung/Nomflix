7b0c79e0054008d684e56120bb98e76827a03809
mixsung <misung@gmail.com>
2018-07-24 03:48:17 +0900

    Bebel but error
    there is erros : npm run build.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,8 +4,23 @@ const devConfig = require("./webpack.dev");
 
 const MODE = process.env.npm_lifecycle_event;
 
+const PATHS = {
+  entry: path.resolve(__dirname, "../src/index.js")
+};
+
 const commonConfig = {
-  entry: "./index.js"
+  entry: ["babel-polyfill", PATHS.entry],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 };
 
 if (MODE === "dev") {
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "output"),
-    filename: "awesome-output.js"
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].js"
   }
 };
