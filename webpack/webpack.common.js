d02c387c9add487485e530babfe287157101d579
mixsung <misung@gmail.com>
2018-07-24 02:44:08 +0900

    Setting webpack dev server
    before babel

---
+++ webpack/webpack.common.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const prodConfig = require("./webpack.prod");
+const devConfig = require("./webpack.dev");
+
+const MODE = process.env.npm_lifecycle_event;
+
+const commonConfig = {
+  entry: "./index.js"
+};
+
+if (MODE === "dev") {
+  module.exports = Object.assign({}, commonConfig, devConfig);
+} else if (MODE === "build") {
+  module.exports = Object.assign({}, commonConfig, prodConfig);
+}
